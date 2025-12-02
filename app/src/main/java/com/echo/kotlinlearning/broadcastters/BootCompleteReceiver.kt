488a0663c712b0b3b0d9e32478a4c3a3507e888c
Estim <2423860442@qq.com>
2025-12-02 11:18:52 +0800

    静态接收广播

---
+++ app/src/main/java/com/echo/kotlinlearning/broadcastters/BootCompleteReceiver.kt
@@ -0,0 +1,15 @@
+package com.echo.kotlinlearning.broadcastters
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+class BootCompleteReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, "Boot Complete", Toast.LENGTH_LONG).show()
+        Log.e("TAG", "Boot Complete")
+    }
+}
